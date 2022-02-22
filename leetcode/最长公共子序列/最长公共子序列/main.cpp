36067c3719633d2c4b4d4ef09b296e1affd116b9
zhy0808 <2423985691@qq.com>
2022-02-22 23:53:34 +0800

    最长公共子序列

---
+++ leetcode/最长公共子序列/最长公共子序列/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int longestCommonSubsequence(string& text1, string& text2)
+{
+	vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1, 0));
+	for (int i = 1; i <= text1.size(); ++i)
+	{
+		for (int j = 1; j <= text2.size(); ++j)
+		{
+			if (text1[i - 1] == text2[j - 1])
+				dp[i][j] = dp[i - 1][j - 1] + 1;
+			else
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+		}
+	}
+	return dp[text1.size()][text2.size()];
+}
+void test()
+{
+	string s1 = "abcde";
+	string s2 = "ace";
+	cout << longestCommonSubsequence(s1, s2) << endl;
+}
+int main()
+{
+	test();
+	return 0;
+}
